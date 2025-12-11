f06b978fce6a3717b9ed10b70fd386f85e28be37
panoskarajohn <panoskarajohn@hotmail.com>
2025-12-11 21:09:14 +0000

    added shared mongo

---
+++ src/shared/Shared.Mongo/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace Shared.Mongo;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMongo(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+
+}
