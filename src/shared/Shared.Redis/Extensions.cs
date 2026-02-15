e3277873f35467d273d6d70b757036c6c550b479
panoskarajohn <panoskarajohn@hotmail.com>
2026-02-15 20:56:04 +0000

    feat: added shared redis

---
+++ src/shared/Shared.Redis/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Shared.Redis;
+
+public static class Extensions {
+    private const string RedisSection = "redis";
+    public static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration) {
+        var section = configuration.GetRequiredSection(RedisSection);
+        var options = new RedisOptions();
+        section.Bind(options);
+        services.Configure<RedisOptions>(section);
+        services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(options.ConnectionString));
+
+        return services;
+    }
+}
+
+public class RedisOptions {
+    public string ConnectionString { get; set; } = string.Empty;
+}
